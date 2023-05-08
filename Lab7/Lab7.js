4fc64c629490e55bf5c66193d0cfc96f0bbecfe1
miramause <115618043+miramause@users.noreply.github.com>
2023-05-08 19:35:01 +0300

    Update Lab7.js

--- Lab7/Lab7.js
+++ Lab7/Lab7.js
@@ -1,224 +1,252 @@
+function Instruction()
+{
+    console.log("                Інструкція\n");
+    console.log("----------------------------------------------");
+    console.log("|   Позначення   |        Що позначає        |");
+    console.log("----------------------------------------------");
+    console.log("|       leg      |            катет          |");
+    console.log("----------------------------------------------");
+    console.log("|   hypotenuse   |         гіпотенуза        |");
+    console.log("----------------------------------------------");
+    console.log("| adjacent angle |  прилеглий до катета кут  |");
+    console.log("----------------------------------------------");
+    console.log("| opposite angle | протилежний до катета кут |");
+    console.log("----------------------------------------------");
+    console.log("|      angle     | один з двох гострих кутів |");
+    console.log("|                | (коли задана гіпотенуза)  |");
+    console.log("----------------------------------------------\n");
+
+    console.log("Відповідно, функція повинна приймати аргументи у такому порядку:");
+    console.log("значення аргумента 1, «тип» аргумента 1 (див. табл), значення");
+    console.log("аргумента 2, «тип» аргумента 2 (див. табл.).");
+    console.log("Н-д:")
+    console.log('triangle(7,"leg",18,"hypotenuse");')
+}
+
 function triangle(value1, type1, value2, type2)
 {
     var pi = Math.PI;
+    var pi_rad = pi/180;
     if(value1==='' || value2==='' || type1==='' || type2==='')
     {
         console.log("Failed");
         console.log("Input all parameters")
     }
+    else if (value1 <= 0 || value2 <= 0)
+    {
+        console.log("Failed");
+        console.log("Zero or negative input");
+    }
     else
     {
-        if(value1===NaN || value2 ===NaN)
+        if(type1==="leg" && type2==="leg")
         {
-            console.log("Failed");
-            console.log("value must be a number");
-        }
-        else
-        {
-            if(value1<=0 || value2<=0)
+            let C = Math.sqrt(Math.pow(value1,2) + Math.pow(value2,2));
+            
+            if((value1+value2>C) && (value1+C>value2) && (value2+C>value1))
             {
-                console.log("Failed");
-                console.log("value must be greater than zero ")
+                console.log("A = " + value1 + '\n' + "B = " + value2+'\n'+"C = " + C);
+                let alpha=Math.atan(value1/value2)/pi_rad;
+
+                let beta=Math.atan(value2/value1)/pi_rad;
+                console.log("alpha = " + alpha + '\n' + "beta = " + beta + '\n' + "gamma = 90");
+                console.log("success");
             }
             else
             {
-                if(type1=="leg" && type2=="leg")
+                console.log("Failed");
+                console.log("Something is wrong with parameters");
+            }
+        }
+        else if((type1==="leg" && type2==="hypotenuse")|| (type2==="leg" && type1==="hypotenuse"))
+        {
+           let B, C;
+           if(type1==="leg")
+            {
+                B=value1;
+                C=value2;
+            }
+            else if(type2==="leg")
+            {
+                C=value1;
+                B=value2;
+            }
+            else
+            {
+                console.log("Failed");
+            }
+
+            if(B>C)
+            {
+                console.log("hypotenuse must be longer than leg")
+                console.log("Failed");
+            }
+            else
+            {
+                let A = Math.sqrt(Math.pow(C,2) - Math.pow(B,2));
+                
+                if((B+C>A) && (B+A>C) && (C+A>B))
                 {
-                    let C = Math.sqrt(Math.pow(value1,2) + Math.pow(value2,2));
-                    console.log("A = " + value1 + '\n' + "B = " + value2+'\n'+"C = " + C);
+                    console.log("A = " + A + '\n' + "B = " + B+'\n'+"C = " + C);
 
-                    let alpha=Math.round(Math.atan(value1/value2)/0.0174532925,1);
-
-                    let beta=Math.round(Math.atan(value2/value1)/0.0174532925,1);
-
-
-                    console.log("alpha = " + alpha + '\n' + "beta = " + beta + '\n' + "hamma = 90");
+                    let alpha=Math.atan(A/B)/pi_rad;
+        
+                    let beta=Math.atan(B/A)/pi_rad;
+                    console.log("alpha = " + alpha + '\n' + "beta = " + beta + '\n' + "gamma = 90");
                     console.log("success");
-                }
-                else if((type1==="leg" && type2==="hypotenuse")|| (type2==="leg" && type1==="hypotenuse"))
-                {
-                    if(type1==="leg")
-                    {
-                        if(value1>value2)
-                        {
-                            console.log("hypotenuse must be longer than leg")
-                            console.log("Failed");
-                        }
-                        else
-                        {
-                            let A = Math.sqrt(Math.pow(value2,2) - Math.pow(value1,2));
-                            console.log("A = " + A + '\n' + "B = " + value1+'\n'+"C = " + value2);
-
-                            let alpha=Math.atan(A/value1)/(pi/180);
-
-                            let beta=Math.atan(value1/A)/(pi/180);
-                            console.log("alpha = " + alpha + '\n' + "beta = " + beta + '\n' + "hamma = 90");
-                            console.log("success");
-                        }
-                    }
-                    else if(type2==="leg")
-                    {
-                        if(value2>value1)
-                        {
-                            console.log("hypotenuse must be longer than leg")
-                            console.log("Failed");
-                        }
-                        else
-                        {
-                            let A = Math.sqrt(Math.pow(value1,2) - Math.pow(value2,2));
-                            console.log("A = " + A + '\n' + "B = " + value2+'\n'+"C = " + value1);
-
-                            let alpha=Math.round(Math.atan(A/value2)/(pi/180),1);
-
-                            let beta=Math.round(Math.atan(value2/A)/(pi/180),1);
-                            console.log("alpha = " + alpha + '\n' + "beta = " + beta + '\n' + "hamma = 90");
-                            console.log("success");
-                        }
-                    }
-                    
-                }
-                else if((type1==="leg" && type2==="adjacent angle")|| (type2==="leg" && type1==="adjacent angle"))
-                {
-                    if(type1==="leg")
-                    {
-                        let C = value1/Math.cos(value2*(pi/180));
-                        let A =value1*Math.tan(value2*(pi/180));
-                        if(A>0&&C>0 && value2<=89)
-                        {
-                            console.log("A = " + A + '\n' + "B = " + value1+'\n'+"C = " + C);
-
-                            let beta=90-value2;
-                            console.log("alpha = " + value2 + '\n' + "beta = " + beta + '\n' + "hamma = 90");
-                            console.log("success");   
-                            }
-                        else
-                           {
-                            console.log("Something wrong with parameters...")
-                            console.log("(angle must be less than 90 )")
-                            console.log("this imposible for right triangle -")
-                            console.log("A = " + A + '\n' + "B = " + value1+'\n'+"C = " + C);
-                            console.log("Failed");
-                        }
-                        
-                    }
-                    else if(type2==="leg")
-                    {
-                        
-                        let C = value2/Math.cos(value1*(pi/180));
-                        let A =value2*Math.tan(value1*(pi/180));
-                        if(A>0&&C>0&& value1<=89)
-                        {
-                            console.log("A = " + A + '\n' + "B = " + value2+'\n'+"C = " + value2);
-
-                            let beta=90-value1;
-                            console.log("alpha = " + value1 + '\n' + "beta = " + beta + '\n' + "hamma = 90");
-                            console.log("success");
-                        }
-                        else
-                        {
-                            console.log("Something wrong with parameters...")
-                            console.log("(angle must be less than 90 )")
-                            console.log("this imposible for right triangle -")
-                            console.log("A = " + A + '\n' + "B = " + value2+'\n'+"C = " + value2);
-                            console.log("Failed");
-                        }
-                        
-                    }
-                }
-                else if((type1==="leg" && type2==="opposite angle")|| (type2==="leg" && type1==="opposite angle"))
-                {
-                    if(type1==="leg")
-                    {
-
-                        let C = value1/Math.sin(value2*(pi/180));
-                        let A = Math.sqrt(C*C-value1*value1);
-                        if(A>0&&C>0 && value2<=89)
-                        {
-                            console.log("A = " + A + '\n' + "B = " + value1+'\n'+"C = " + C);
-
-                            let beta=90-value2;
-                            console.log("alpha = " + value2 + '\n' + "beta = " + beta + '\n' + "hamma = 90");
-                            console.log("success");   
-                            }
-                        else
-                           {
-                            console.log("Something wrong with parameters...")
-                            console.log("(angle must be less than 90 )");
-                            console.log("this imposible for right triangle -")
-                            console.log("A = " + A + '\n' + "B = " + value1+'\n'+"C = " + C);
-                            console.log("Failed");
-                        }
-                         
-                    }
-                    else if(type2==="leg")
-                    {
-                        let C = value2/Math.sin(value1*(pi/180));
-                        let A = Math.sqrt(C*C-value2*value2);
-                        if(A>0&&C>0&& value1<=89)
-                        {
-                            console.log("A = " + A + '\n' + "B = " + value2+'\n'+"C = " + C);
-
-                            let beta=90-value1;
-                            console.log("alpha = " + value1 + '\n' + "beta = " + beta + '\n' + "hamma = 90");
-                            console.log("success");
-                        }
-                        else
-                        {
-                            console.log("Something wrong with parameters...")
-                            console.log("(angle must be less than 90 )");
-                            console.log("this imposible for right triangle -")
-                            console.log("A = " + A + '\n' + "B = " + value2+'\n'+"C = " + value2);
-                            console.log("Failed");
-                        }
-                    }
-                }
-                else if((type1==="angle" && type2==="hypotenuse")|| (type2==="angle" && type1==="hypotenuse"))
-                {
-                    if(type1==="angle")
-                    {
-                        if(value1>89)
-                        {
-                            console.log("angle must be less than 90 ");
-                            console.log("Failed");
-                        }
-                        else
-                        {
-                            let A = value2*Math.sin(value1*(pi/180));
-                            let B = value2*Math.cos(value1*(pi/180))
-                            console.log("A = " + A + '\n' + "B = " + B+'\n'+"C = " + value2);
-
-
-                            let beta=90-value1;
-                            console.log("alpha = " + value1 + '\n' + "beta = " + beta + '\n' + "hamma = 90");
-                            console.log("success");
-                        }
-                    }
-                    else if(type2==="angle")
-                    {
-                        if(value2>89)
-                        {
-                            console.log("angle must be less than 90 ");
-                            console.log("Failed");
-                        }
-                        else
-                        {
-                            let A = value1*Math.sin(value2*(pi/180));
-                            let B = value1*Math.cos(value2*(pi/180))
-                            console.log("A = " + A + '\n' + "B = " + B+'\n'+"C = " + value1);
-
-
-                            let beta=90-value2;
-                            console.log("alpha = " + value2 + '\n' + "beta = " + beta + '\n' + "hamma = 90");
-                            console.log("success");
-                        }
-                    }
                 }
                 else
                 {
                     console.log("Failed");
-                    console.log("Incorrect parameters");
+                    console.log("Something is wrong with parameters");
                 }
             }
         }
+
+        else if((type1==="leg" && type2==="adjacent angle")|| (type2==="leg" && type1==="adjacent angle"))    
+        {
+            let B, alpha;
+            if(type1==="leg")
+            {
+                B=value1;
+                alpha=value2;
+            }
+            else if(type2==="leg")
+            {
+                B=value2;
+                alpha=value1;
+            }
+            else
+            {
+                console.log("Failed");
+            }
+
+            if(alpha>=90)
+            {
+                console.log("adjacent angle must be less than 90");
+                console.log("Failed");
+            }
+            else
+            {
+                let C = B/Math.cos(alpha*pi_rad);
+                let A =B*Math.tan(alpha*pi_rad);
+
+                if((B+C>A) && (B+A>C) && (C+A>B))
+                {
+                    console.log("A = " + A + '\n' + "B = " + B+'\n'+"C = " + C);
+                    let beta=90-alpha;
+                    console.log("alpha = " + alpha + '\n' + "beta = " + beta + '\n' + "gamma = 90");
+                    console.log("success");
+                }
+                else
+                {
+                    console.log("Failed");
+                    console.log("Something is wrong with parameters");
+                }
+            }
+        }       
+        else if((type1==="leg" && type2==="opposite angle")|| (type2==="leg" && type1==="opposite angle"))
+        {
+            let B, beta;
+            if(type1==="leg")
+            {
+                B=value1;
+                beta=value2;
+            }
+            else if(type2==="leg")
+            {
+                B=value2;
+                beta=value1;
+            }
+            else
+            {
+                console.log("Failed");
+            }
+
+            if(beta>=90)
+            {
+                console.log("opposite angle must be less than 90");
+                console.log("Failed");
+            }
+            else
+            {
+                let C = B/Math.sin(beta*(pi_rad));
+                let A = Math.sqrt(C*C-B*B);
+
+                if((B+C>A) && (B+A>C) && (C+A>B))
+                {
+                    console.log("A = " + A + '\n' + "B = " + B+'\n'+"C = " + C);
+                    let alpha=90-beta;
+                    console.log("alpha = " + alpha + '\n' + "beta = " + beta + '\n' + "gamma = 90");
+                    console.log("success");
+                }
+                else
+                {
+                    console.log("Failed");
+                    console.log("Something is wrong with parameters");
+                }
+            }
+
+        }
+        else if((type1==="angle" && type2==="hypotenuse")|| (type2==="angle" && type1==="hypotenuse"))
+        {
+            let C, beta;
+            if(type1==="angle")
+            {
+                beta=value1;
+                C=value2;
+            }
+            else if(type2==="angle")
+            {
+                beta=value2;
+                C=value1;
+            }
+            else
+            {
+                console.log("Failed");
+            }
+
+            if(beta>=90)
+            {
+                console.log("angle must be less than 90");
+                console.log("Failed");
+            }
+            else
+            {
+                let A = C*Math.sin(beta*(pi_rad));
+                let B = C*Math.cos(beta*(pi_rad));
+
+                if((B+C>A) && (B+A>C) && (C+A>B))
+                {
+                    console.log("A = " + A + '\n' + "B = " + B+'\n'+"C = " + C);
+                    let alpha=90-beta;
+                    console.log("alpha = " + alpha + '\n' + "beta = " + beta + '\n' + "gamma = 90");
+                    console.log("success");
+                }
+                else
+                {
+                    console.log("Failed");
+                    console.log("Something is wrong with parameters");
+                }
+            }
+        }
+        else if(type1 !== "leg" || type1 !== "hypotenuse" || type1 !== "adjacent angle" || type1 !== "opposite angle" || type1 !== "angle" ||
+        type2 !== "leg" || type2 !== "hypotenuse" || type2 !== "adjacent angle" || type2 !== "opposite angle" || type2 !== "angle")
+        {
+            console.log("Instruction!!!!");
+        }
     }
 }
+Instruction();
+
+//triangle(7,"leg",18,"hypotenuse");
+//triangle(18,"hypotenuse",7,"leg");
+
+//triangle(7,"leg",18,"adjacent angle");
+//triangle(18,"adjacent angle", 7,"leg");
+
+//triangle(60,"opposite angle", 5,"leg");
+//triangle(5,"leg",60,"opposite angle");
+
+//triangle(60,"angle", 5,"hypotenuse");
+//triangle(5,"hypotenuse",60,"angle");
