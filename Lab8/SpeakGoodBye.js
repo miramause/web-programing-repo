f53e69e153bed6e10507db0c3b69876a5bdc1222
miramause <115618043+miramause@users.noreply.github.com>
2023-05-11 11:05:12 +0300

    Update SpeakGoodBye.js

--- Lab8/SpeakGoodBye.js
+++ Lab8/SpeakGoodBye.js
@@ -1,4 +1,13 @@
-var Bye = function (name) {
-  var speakWord = "Good Bye";
-  console.log(speakWord + " " + name);
+var Bye2 ={};
+Bye2.speakWord2  = "Good Bye";
+
+Bye2.Bye = function (name) 
+{
+  console.log(Bye2.speakWord2 + " " + name);
 }
+
+// var Bye = function (name) 
+// {
+//   var speakWord = "Good Bye";
+//   console.log(speakWord + " " + name);
+// }
