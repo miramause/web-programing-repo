1bb649b67420b76c962bfa03dfdc6bd148d72a4d
miramause <115618043+miramause@users.noreply.github.com>
2023-05-11 11:05:37 +0300

    Update SpeakHello.js

--- Lab8/SpeakHello.js
+++ Lab8/SpeakHello.js
@@ -1,4 +1,14 @@
-var Hello = function (name) {
-  var speakWord = "Hello";
-  console.log(speakWord + " " + name);
+var Hello2 ={};
+Hello2.speakWord  = "Good Bye";
+
+Hello2.Hello = function (name) 
+{
+  console.log(Hello2.speakWord + " " + name);
 }
+
+
+// var Hello = function (name) 
+// {
+//   var speakWord = "Hello";
+//   console.log(speakWord + " " + name);
+// }
